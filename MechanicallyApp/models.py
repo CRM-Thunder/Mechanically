f255918a800d987cb70fbdcd35448092cd6f3433
CRM-Thunder <damian-zamecki@wp.pl>
2025-07-09 22:24:17 +0200

    added RepairReport, FailureReport, UserLocationAssignment, Location models, added UUID as primary key format for models

--- MechanicallyApp/models.py
+++ MechanicallyApp/models.py
@@ -1,9 +1,10 @@
 from django.core.validators import MinLengthValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
+import uuid
 class User(AbstractUser):
     #odwzorować mechanizm tworzenia username jak na usosie, czyli 3 litery imienia 3 litery nazwiska i 4 cyfrowy kod
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     username = models.CharField(max_length=10, unique=True,
     validators=[
         MinLengthValidator(10)
@@ -58,7 +59,7 @@ class Vehicle(models.Model):
         AVAILABLE = 'A'
         UNAVAILABLE='U'
         REPAIR = 'R'
-
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
     vin=models.CharField(max_length=17,unique=True, validators=[MinLengthValidator(17)])
     manufacturer=models.CharField(max_length=20)
     vehicleModel=models.CharField(max_length=20)
@@ -69,4 +70,48 @@ class Vehicle(models.Model):
     fuelType=models.CharField(max_length=2, choices=FuelTypeChoices.choices, default=FuelTypeChoices.OTHER)
     # noinspection PyUnresolvedReferences
     availability=models.CharField(max_length=1, choices=AvailabilityChoices.choices, default=AvailabilityChoices.AVAILABLE)
+    #musi być zabezpieczenie że może być przypisany tylko do brancha
+    branchId=models.ForeignKey('Location',on_delete=models.CASCADE)
 
+class Location(models.Model):
+    class LocationTypeChoices(models.TextChoices):
+        BRANCH='B'
+        WORKSHOP='W'
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    #narazie jako pojedyncze pole, w razie czego można rozbudować
+    address=models.CharField(max_length=50)
+    # noinspection PyUnresolvedReferences
+    locationType=models.CharField(max_length=1,choices=LocationTypeChoices.choices,default=LocationTypeChoices.BRANCH)
+
+#należy wprowadzić zabezpieczenie, że do warsztatu może zostać przydzielony tylko mechanik, a do brancha tylko standardowy pracownik
+class UserLocationAssignment(models.Model):
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    userId=models.ForeignKey(User,on_delete=models.CASCADE)
+    locationId=models.ForeignKey(Location,on_delete=models.CASCADE)
+    assignDate=models.DateTimeField(auto_now_add=True)
+
+class FailureReport(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING='P'
+        ASSIGNED='A'
+        DISMISSED='D'
+        RESOLVED='R'
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    vehicleId=models.ForeignKey(Vehicle,on_delete=models.CASCADE)
+    description=models.TextField()
+    reportDate=models.DateTimeField(auto_now_add=True)
+    #zabezpieczyć aby tylko standardowy/menadżer mógł być autorem
+    reportAuthor=models.ForeignKey(User,on_delete=models.CASCADE)
+    # noinspection PyUnresolvedReferences
+    status=models.CharField(max_length=1,choices=StatusChoices.choices,default=StatusChoices.PENDING)
+    lastStatusChangeDate=models.DateTimeField(auto_now=True)
+
+class RepairReport(models.Model):
+
+    id=models.UUIDField(primary_key=True,default=uuid.uuid4,editable=False)
+    failureReportId=models.ForeignKey(FailureReport,on_delete=models.CASCADE)
+    conditionAnalysis=models.TextField()
+    repairAction=models.TextField()
+    cost=models.DecimalField(max_digits=8,decimal_places=2)
+    reportDate=models.DateTimeField(auto_now_add=True)
+    readyForReview=models.BooleanField(default=False)
